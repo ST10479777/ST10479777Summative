7cb57f92c00c87ff23b57fdc6ecd8e90d1ccba35
ST10479777 <ST10479777@vcconnect.edu.za>
2025-06-19 08:58:08 +0200

    used and if statement and added options to playlist

--- app/src/main/java/za/co/varsitycollege/st19479777/st10479777summative/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st19479777/st10479777summative/MainActivity.kt
@@ -3,6 +3,7 @@ package za.co.varsitycollege.st19479777.st10479777summative
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     companion object {
         val song = ArrayList<String>()
         val artist = ArrayList<String>()
-        val rating = ArrayList<Int>()
+        val rating = ArrayList<String>()
         val comment = ArrayList<String>()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +21,39 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val title = findViewById<EditText>(R.id.SongTitle)
-        val name = findViewById<EditText>(R.id.ArtistName)
+        val titleTxt = findViewById<EditText>(R.id.SongTitle)
+        val nameTxt = findViewById<EditText>(R.id.ArtistName)
         val rateTxt = findViewById<EditText>(R.id.RatingTxt)
-        val comments = findViewById<EditText>(R.id.CommentTxt)
+        val commentsTxt = findViewById<EditText>(R.id.CommentTxt)
 
         val add = findViewById<Button>(R.id.AddBtn)
         val view = findViewById<Button>(R.id.ViewListBtn)
         val exit = findViewById<Button>(R.id.ExitBtn)
+
+        add.setOnClickListener {
+            val title = titleTxt.text.toString()
+            val name = nameTxt.text.toString()
+            val rate = rateTxt.text.toString()
+            val comments = commentsTxt.text.toString()
+
+            if (title.isEmpty() || name.isEmpty() || rate.isEmpty ()) {
+                Toast.makeText(this, "Please enter all fields", Toast.LENGTH_SHORT).show()
+                } else {
+                    val commentTxt = comments.toIntOrNull()
+                if (commentTxt == null  || commentTxt < 0 || commentTxt > 5) {
+                    Toast.makeText(this, "Please enter valid rating", Toast.LENGTH_SHORT).show()
+
+            } else {
+                song.add(title)
+                artist.add(name)
+                rating.add(rate)
+                comment.add(comments)
+                    Toast.makeText(this, "Options have been added to playlist", Toast.LENGTH_SHORT).show()
+
+
+                }
+
+        }
 
 
     }
