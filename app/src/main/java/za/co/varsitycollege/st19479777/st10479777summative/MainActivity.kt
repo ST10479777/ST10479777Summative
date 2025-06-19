2fa2cac3fa1c0309881a356540660b33ccf05de1
ST10479777 <ST10479777@vcconnect.edu.za>
2025-06-19 08:32:18 +0200

    designed Layout and declared variables

--- app/src/main/java/za/co/varsitycollege/st19479777/st10479777summative/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st19479777/st10479777summative/MainActivity.kt
@@ -1,20 +1,34 @@
 package za.co.varsitycollege.st19479777.st10479777summative
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val song = ArrayList<String>()
+        val artist = ArrayList<String>()
+        val rating = ArrayList<Int>()
+        val comment = ArrayList<String>()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val title = findViewById<EditText>(R.id.SongTitle)
+        val name = findViewById<EditText>(R.id.ArtistName)
+        val rateTxt = findViewById<EditText>(R.id.RatingTxt)
+        val comments = findViewById<EditText>(R.id.CommentTxt)
+
+        val add = findViewById<Button>(R.id.AddBtn)
+        val view = findViewById<Button>(R.id.ViewListBtn)
+        val exit = findViewById<Button>(R.id.ExitBtn)
+
+
     }
 }
