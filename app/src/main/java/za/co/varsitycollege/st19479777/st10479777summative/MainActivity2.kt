98c132a4cbfcf9f68d33d9ff0850a4a9c1af331b
ST10479777 <ST10479777@vcconnect.edu.za>
2025-06-19 09:47:19 +0200

    used a for loop to display outputs

--- app/src/main/java/za/co/varsitycollege/st19479777/st10479777summative/MainActivity2.kt
+++ app/src/main/java/za/co/varsitycollege/st19479777/st10479777summative/MainActivity2.kt
@@ -1,6 +1,8 @@
 package za.co.varsitycollege.st19479777.st10479777summative
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,26 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+
+        val display = findViewById<TextView>(R.id.DisplayTxt)
+        val back = findViewById<Button>(R.id.BackBtn)
+        val ratings =findViewById<Button>(R.id.RatingBtn)
+
+
+        ratings.setOnClickListener {
+            val output = StringBuilder()
+            for (i in MainActivity.song.indices) {
+                output.append("Song Title: ${MainActivity.song[i]} \n")
+                output.append("Name of Artist: ${MainActivity.artist[i]} \n")
+                output.append("Rating: ${MainActivity.rating[i]} \n")
+                output.append("Comment: ${MainActivity.comment[i]} \n")
+                output.append("------------------------ \n")
+            }
+
+
+        }
+
+
 
     }
 }
